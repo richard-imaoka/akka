f26b3be99b930f065f3aab6b2c622355032bc19d
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-11-16 07:47:32 +0900

    WIP

--- akka-docs/src/test/scala/docs/stream/SubstreamDocSpec.scala
+++ akka-docs/src/test/scala/docs/stream/SubstreamDocSpec.scala
@@ -46,6 +46,22 @@ class SubstreamDocSpec extends AkkaSpec {
 
     Source(1 to 10).splitAfter(SubstreamCancelStrategy.drain)(_ == 3)
     //#splitWhenAfter
+
+    //#wordCount
+    val text =
+      """This is the first line.
+        |The second line.
+        |There is also the 3rd line."""
+        .stripMargin
+
+    val charCount = Source(text.toList)
+      .splitAfter { _ == '\n' }
+      .filter(_ != '\n')
+      .map(_ => 1)
+      .reduce(_ + _)
+      .to(Sink.foreach(println))
+      .run()
+    //#wordCount
   }
 
   "generate substreams by flatMapConcat and flatMapMerge" in {
